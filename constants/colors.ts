0d9dac2fb2b26b484ed87568a150cbbd0af6c1a7
xianxin <1418436581@qq.com>
2026-04-15 14:40:34 +0800

    Restore shared colors config

---
+++ constants/colors.ts
@@ -0,0 +1,61 @@
+const Colors = {
+  primary: '#0082EF',
+  primaryLight: '#E6F4FF',
+  primaryDark: '#0066CC',
+  primarySurface: '#F0F9FF',
+
+  accent: '#FA8C16',
+  accentLight: '#FFF7E6',
+
+  background: '#F5F6F7',
+  backgroundSecondary: '#EBEEF5',
+  backgroundTertiary: '#E8EAEF',
+  card: '#FFFFFF',
+  cardElevated: '#FFFFFF',
+
+  text: '#171A1D',
+  textSecondary: '#5E6D82',
+  textTertiary: '#8F9BB3',
+  textInverse: '#FFFFFF',
+  textMuted: '#8F9BB3',
+
+  border: '#EBEEF5',
+  borderLight: '#E8EAEF',
+  divider: '#D9D9D9',
+
+  success: '#52C41A',
+  successLight: '#F6FFED',
+  successSurface: '#F6FFED',
+  warning: '#FAAD14',
+  warningLight: '#FFFBE6',
+  warningSurface: '#FFFBE6',
+  danger: '#FF4D4F',
+  dangerLight: '#FFF2F0',
+  dangerSurface: '#FFF2F0',
+  info: '#0082EF',
+  infoLight: '#E6F4FF',
+  infoSurface: '#E6F4FF',
+
+  progress: {
+    high: '#52C41A',
+    medium: '#FAAD14',
+    low: '#FF4D4F',
+  },
+
+  score: {
+    excellent: '#52C41A',
+    good: '#0082EF',
+    fair: '#FAAD14',
+    poor: '#FF4D4F',
+  },
+
+  tabIconDefault: '#8F9BB3',
+  tabIconSelected: '#0082EF',
+  tint: '#0082EF',
+
+  white: '#FFFFFF',
+  black: '#000000',
+  transparent: 'transparent',
+};
+
+export default Colors;
